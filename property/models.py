36ce69f28fb0c05b90b5ac7000f61c95bd72f2bd
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-10-03 10:09:06 +0300

    add Claims, Owner

--- property/models.py
+++ property/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    created_at = models.DateTimeField(
-        'Когда создано объявление',
-        default=timezone.now,
-        db_index=True)
-
+    owner = models.CharField('ФИО владельца', blank=False, max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', blank=False, max_length=20)
+    owner_pure_phone = PhoneNumberField('Правильный номер владельца', region='RU', blank=True, max_length=20)
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
 
@@ -46,7 +44,45 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
-    new_building = models.BooleanField(null=True)
+    new_building = models.BooleanField(blank=True)
+    liked_by = models.ManyToManyField(User,
+                                      verbose_name='Кто лайкнул',
+                                      blank=True)
+    created_at = models.DateTimeField(
+        'Когда создано объявление',
+        default=timezone.now,
+        db_index=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Claims(models.Model):
+    user = models.ForeignKey(User,
+                             verbose_name='Кто жаловался',
+                             on_delete=models.CASCADE,
+                             blank=False)
+    flat = models.ForeignKey(Flat,
+                              verbose_name="Квартира, на которую жаловались",
+                              on_delete=models.CASCADE)
+    text = models.TextField(verbose_name="Текст", blank=True)
+
+
+class Owner(models.Model):
+    owner = models.CharField('ФИО владельца',
+                             blank=False,
+                             max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца',
+                                          blank=False,
+                                          max_length=20)
+    owner_pure_phone = PhoneNumberField('Правильный номер владельца',
+                                        region='RU',
+                                        blank=True,
+                                        max_length=20)
+    flats = models.ManyToManyField(Flat,
+                                  related_name='owners',
+                                  verbose_name='Квартиры в собственности')
+
+    def __str__(self):
+        return f'{self.owner}'
+
