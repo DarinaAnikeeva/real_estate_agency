1c614060457eb9e257b9cc46e4852d0fd93aa0a0
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-10-04 09:01:39 +0300

    add MembershipInline

--- property/admin.py
+++ property/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import *
 
+class MembershipInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ['owner']
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ['created_at']
@@ -9,12 +13,18 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ['liked_by']
+    inlines = [
+        MembershipInline
+    ]
 
 class ClaimsAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'flat']
 
+
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['flats']
+
+
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Claims, ClaimsAdmin)
