a631b7bb9120f9eae364c911edc1cfca003b73de
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-10-04 09:31:01 +0300

    change to admin.register()

--- property/admin.py
+++ property/admin.py
@@ -6,10 +6,11 @@ class MembershipInline(admin.TabularInline):
     model = Owner.flats.through
     raw_id_fields = ['owner']
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'pure_phone')
     list_editable = ['new_building']
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ['liked_by']
@@ -17,15 +18,12 @@ class FlatAdmin(admin.ModelAdmin):
         MembershipInline
     ]
 
+@admin.register(Claims)
 class ClaimsAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'flat']
 
-
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['flats']
 
 
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Claims, ClaimsAdmin)
-admin.site.register(Owner, OwnerAdmin)
